3cf4af4c18e5cc3eee5aad949246589583050a46
netelli <natalli.dev@gmail.com>
2019-10-22 17:30:17 +0300

    Displaying user ID and Roles;

--- src/main/java/com/study/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/study/springsecurity/demo/config/DemoSecurityConfig.java
@@ -16,8 +16,8 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
         User.UserBuilder users = User.withDefaultPasswordEncoder();
         auth.inMemoryAuthentication()
                 .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-                .withUser(users.username("marry").password("test123").roles("MANAGER"))
-                .withUser(users.username("susan").password("test123").roles("ADMIN"));
+                .withUser(users.username("marry").password("test123").roles("EMPLOYEE", "MANAGER"))
+                .withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
     }
 
     @Override
--- src/main/webapp/WEB-INF/view/home.jsp
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -6,6 +6,7 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <html>
 <head>
     <title>Home Page</title>
@@ -14,7 +15,15 @@
 <h2>Home page</h2>
 <hr/>
 <p>Welcome to Home page</p>
+<hr/>
+<p>
+    User: <security:authentication property="principal.username"/>
+    <br/><br/>
+    Role(s): <security:authentication property="principal.authorities"/>
+</p>
 
+
+<hr/>
 <form:form action="${pageContext.request.contextPath}/logout"
            method="post">
     <input type="submit" value="Logout"/>
